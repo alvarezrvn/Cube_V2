c27f70ae897fa370ecc8b2d8aace7b07c689179d
raven.alv <araven@nmsu.edu>
2019-05-05 17:47:54 -0600

    added 3x3 and extra objects

--- Cube Project/Assets/Scripts/MainMenu.cs
+++ Cube Project/Assets/Scripts/MainMenu.cs
@@ -10,6 +10,11 @@ public class MainMenu : MonoBehaviour
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
+    public void PlayGame3x3()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 2);
+    }
+
     public void QuitGame()
     {
         Debug.Log("Session has Ended");
